600d73fa4787b07ad228e447fd50287d95aaf13a
Boris Brovkin <borisimusb@yandex.ru>
2022-08-22 12:04:33 +0500

    New

---
+++ Seminar_1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+int a = -9;
+int b = -3;
+if (b > a) Console.WriteLine("b является большим числом, a - меньшим");
+else Console.WriteLine("a является большим числом, b - меньшим");
